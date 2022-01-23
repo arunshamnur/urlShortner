770ac0699d2464cef35102a4d242f60754b3763a
arunshamnur <arunshamnur08@gmail.com>
2022-01-23 04:48:20 -0800

    Added logic to return all stored shorturl's

--- urlShortner.go
+++ urlShortner.go
@@ -79,14 +79,14 @@ func shortUrl(w http.ResponseWriter, r *http.Request) {
 
 func returnAllShortenedUrl(w http.ResponseWriter, r *http.Request){
 //	function for returning all previous shortened url's
-
+	json.NewEncoder(w).Encode(urlStructs)
 }
 
 func apiRequests() {
 	//api request definitions for handling url shortning
 	route := mux.NewRouter().StrictSlash(true)
-	route.HandleFunc("/shortUrl", shortUrl).Methods("POST")
-	route.HandleFunc("/getUrl", returnAllShortenedUrl).Methods("GET")
+	route.HandleFunc("/", shortUrl).Methods("POST")
+	route.HandleFunc("/", returnAllShortenedUrl).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", route))
 }
 
@@ -107,4 +107,5 @@ func main() {
 	}
 	apiRequests()
 }
+
 
